4287bab8fbcde8ef3714dccd44963478babe1f2c
DerRiese <DerRiese765@gmail.com>
2023-04-20 06:23:54 +0300

    -

--- main.js
+++ main.js
@@ -8,9 +8,25 @@ color = 'black';
 console.log(color);
 console.log(color);
 console.log(color);
+
 //comment
+
 let name = 'Nasser';
 let age = 30;
 console.log(typeof name);
 console.log(typeof age);
+
+let result = 2 > 3;
+console.log(result);
+console.log(typeof result);
+
+let message = "Hello world";
+console.log(typeof message);
+console.log(message);
+
+let name1;
+console.log(name1);
+
+let value = 2;
+console.log(value * 'JavaScript');
 
